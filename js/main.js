74d2f51598550e08015242804d48a5b7c52b914c
UyenQuyen <uynwiin@gmail.com>
2023-02-28 22:10:42 +0700

    Update header + responsive

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const changeButtonMenu = (x) => {
+    x.classList.toggle("change");
+}
+
+const goToHome = () =>{
+    window.location = "http://127.0.0.1:5500/pages/index.html"
+}
+
+const goToShop = () =>{
+    window.location = "http://127.0.0.1:5500/pages/shop.html"
+}
+
+const goToAboutUs = () =>{
+    window.location = "http://127.0.0.1:5500/pages/about_us.html"
+}
+
+const goToCart = () => {
+    window.location = "http://127.0.0.1:5500/pages/cart.html"
+}
